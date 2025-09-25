985eea76ff5942fa5a93eb304804ab19ed90a6e2
waltano <waltano@mail.com>
2025-09-25 11:36:20 +0300

    1

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,10 @@ REST_FRAMEWORK = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "127.0.0.1",
+        "NAME": "db",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "db",
+        "HOST": "127.0.0.1",
         "PORT": 5432,
     }
 }
